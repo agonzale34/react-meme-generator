91ba51149ff1b7dfa6a1cb373906c2baf47725e1
Anthony Gonzalez <agonzalez@lixar.com>
2020-05-14 15:14:51 -0300

    Updates

---
+++ precache-manifest.ea05e586b2da907c4da9559d4bb327db.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "feff1613601f52c2d8afab5e64cea7d7",
+    "url": "/react-meme-generator/index.html"
+  },
+  {
+    "revision": "361b1b98ca9021ee6327",
+    "url": "/react-meme-generator/static/css/main.08163736.chunk.css"
+  },
+  {
+    "revision": "55b2b615971c5890fb41",
+    "url": "/react-meme-generator/static/js/2.fbd0305d.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/react-meme-generator/static/js/2.fbd0305d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "361b1b98ca9021ee6327",
+    "url": "/react-meme-generator/static/js/main.ca5ce9d1.chunk.js"
+  },
+  {
+    "revision": "188cccbde0e9adcb3090",
+    "url": "/react-meme-generator/static/js/runtime-main.cd190795.js"
+  }
+]);
